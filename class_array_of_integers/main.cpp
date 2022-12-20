3b5582775651e51c128bf6359ba52ccc18a3df39
filian <filiandun@gmail.com>
2022-12-20 12:33:04 +0300

    main (3): 90% ready

--- class_array_of_integers/main.cpp
+++ class_array_of_integers/main.cpp
@@ -11,6 +11,15 @@ int main()
     unsigned long long int array_length_2 = 5;
     int* array_of_integers_2 = new int[array_length_2 + 1] { 0, 22, 11, 3, 63, '\0' };
 
+    //unsigned long long int array_length_1 = 10;
+    //int* array_of_integers_1 = new int[array_length_1 + 1] { 5, 0, 342, 44, 5921, 2, 55, 276, 89, 345, '\0' };
+
+    //unsigned long long int array_length_2 = 7;
+    //int* array_of_integers_2 = new int[array_length_2 + 1] { 0, 276, 111, 33, 639, 5, 266, '\0' };
+
+    //unsigned long long int array_length_3 = 7;
+    //int* array_of_integers_3 = new int[array_length_3 + 1] { 276, 639, 0, 266, 5, 111, 33, '\0' };
+
 // ДОБАВЛЕНИЕ ЭЛЕМЕНТА ВО МНОЖЕСТВО (РАБОТАЕТ)
     // ПЕРЕГРУЗКА + (РАБОТАЕТ)
     //Array array_1(array_of_integers_1, array_length_1);
@@ -26,21 +35,29 @@ int main()
     //array_1 += 6; // 6 - отсутствует, добавится
     //std::cout << array_1 << std::endl;
 
-// ОБЪЕДИНЕНИЕ МНОЖЕСТВ (РАБОТАЕТ ЧАСТИЧНО)
-    // ПЕРЕГРУЗКА + (РАБОТАЕТ ЧАСТИЧНО)
+// ОБЪЕДИНЕНИЕ МНОЖЕСТВ (РАБОТАЕТ)
+    // ПЕРЕГРУЗКА + (РАБОТАЕТ)
     //Array array_1(array_of_integers_1, array_length_1);
     //Array array_2(array_of_integers_2, array_length_2);
 
-    //array_1 + array_2;
-    //std::cout << array_1 << std::endl;
-
-
     //std::cout << array_1 + array_2 << std::endl;
+    //std::cout << array_1 << std::endl; // не изменится
+    //std::cout << array_2 << std::endl; // не изменится
    
-    // ПЕРЕГРУЗКА += (НЕ ДЕЛАЛ)
+    // ПЕРЕГРУЗКА += (РАБОТАЕТ)
+    //Array array_1(array_of_integers_1, array_length_1);
+    //Array array_2(array_of_integers_2, array_length_2);
 
+    //array_1 += array_2;
+    //std::cout << array_1 << std::endl; // изменится
+    //std::cout << array_2 << std::endl; // не изменится
 
-// ОБЪЕДИНЕНИЕ МНОЖЕСТВ
+    //std::cout << std::endl;
+
+    //array_2 += array_1;
+    //std::cout << array_1 << std::endl; // не изменится
+    //std::cout << array_2 << std::endl; // изменится
+
 
 // УДАЛЕНИЕ ЭЛЕМЕНТА ИЗ МНОЖЕСТВА (РАБОТАЕТ)
     // ПЕРЕГРУЗКА - (РАБОТАЕТ)
@@ -97,14 +114,15 @@ int main()
     //array_1 *= array_2;
     //std::cout << array_1 << std::endl;
 
-// ПРИСВАИВАНИЕ
-    Array array_1(array_of_integers_1, array_length_1);
-    Array array_2;
-    array_2 = array_1;
+// ПРИСВАИВАНИЕ (РАБОТАЕТ)
+    //Array array_1(array_of_integers_1, array_length_1);
+    //Array array_2(array_of_integers_2, array_length_2);
+    //array_2 = array_1;
 
-    std::cout << array_2 << std::endl;
+    //std::cout << array_1 << std::endl;
+    //std::cout << array_2 << std::endl;
 
-// СРАВНЕНИЕ МНОЖЕСТВ
+// СРАВНЕНИЕ МНОЖЕСТВ (РАБОТАЕТ)
     //Array array_1(array_of_integers_1, array_length_1);
     //Array array_2(array_of_integers_2, array_length_2);
     //if (array_1 == array_2)
@@ -129,14 +147,10 @@ int main()
     //    std::cout << "Массивы не равны" << std::endl;
     //}
 
-// ПЕРЕГРУЗКА << И >> (РАБОТАЕТ)
-    //Array* a = new Array;
-    //cin >> a;
-    //std::cout << a << std::endl;
-
-    //Array a2;
-    //cin >> a2;
-    //std::cout << a2 << std::endl;
+// ПЕРЕГРУЗКА << И >> (РАБОТАЕТ ЧАСТИЧНО)
+    Array a2;
+    cin >> a2;
+    std::cout << a2 << std::endl;
 
 
     return 0;
